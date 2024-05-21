e95aa5c07ebba48a6c9e35e4e75871d99a0f4e2a
Linus Fujisawa <92174981+2fujisawa@users.noreply.github.com>
2024-05-21 16:02:00 -0700

    Initial commit

---
+++ DataStructures - CPP/preDSAmath.h
@@ -0,0 +1,6 @@
+using namespace std; 
+
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ DataStructures - CPP/preDSAmessage.h
@@ -0,0 +1,6 @@
+using namespace std; 
+
+void Log(const char* message) //function that inputs a string //definition
+{
+    cout << message << endl;
+}
