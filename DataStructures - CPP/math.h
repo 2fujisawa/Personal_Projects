0d2ac8d9160a661fdff7ec6d4db3590189e6c866
Linus Fujisawa <fujisawa@chapman.edu>
2024-05-23 15:37:56 -0700

    Changes

--- DataStructures - CPP/math.h
+++ DataStructures - CPP/math.h
@@ -1,8 +1,11 @@
+<<<<<<< HEAD:DataStructures - CPP/math.h
 #pragma once //Allows the compilation to recognize that the header file is only included once during the process
             //for example we use the #include "preDSAmessage.h" so it is usable again here  
             //which is going to be also called in the main preDSA.cpp file, by stating pragma once it makes sure that it is only called once during compilation
 #include "message.h" //Including a header file
 
+=======
+>>>>>>> parent of 139ac2e (updates):DataStructures - CPP/preDSAmath.h
 using namespace std; 
 
 int add(int a, int b)
--- DataStructures - CPP/message.h
+++ DataStructures - CPP/message.h
@@ -1,8 +1,11 @@
+<<<<<<< HEAD:DataStructures - CPP/message.h
 #pragma once//Allows the compilation to recognize that the header file is only included once during the process
 
 #include <iostream>
 #include "math.h" // include another header fil
 
+=======
+>>>>>>> parent of 139ac2e (updates):DataStructures - CPP/preDSAmessage.h
 using namespace std; 
 
 void Log(const char* message) //function that inputs a string //definition
